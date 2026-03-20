b364be4c37e7f13815eceb387254dfe0436583f4
Egydio <egydiio@hotmail.com>
2026-03-19 22:52:01 -0300

    feat: add soft deletes to products table and model

--- database/migrations/2026_03_20_004225_create_products_table.php
+++ database/migrations/2026_03_20_004225_create_products_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->decimal('price', 10, 2);
             $table->integer('stock_quantity');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
